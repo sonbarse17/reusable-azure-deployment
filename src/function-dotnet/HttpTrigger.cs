0a4f9ae80d1218da2b676c9a6ece407a923c442f
sonbarse17 <sushantsonbarse90@gmail.com>
2026-03-19 11:57:24 +0530

    Add reusable Azure deployment workflows and sample apps

---
+++ src/function-dotnet/HttpTrigger.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace function_dotnet
+{
+    public class HttpTrigger
+    {
+        private readonly ILogger _logger;
+
+        public HttpTrigger(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<HttpTrigger>();
+        }
+
+        [Function("HttpTrigger")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to .NET 8 Isolated Azure Function!");
+
+            return response;
+        }
+    }
+}
---
+++ src/function-node/src/functions/httpTrigger.js
@@ -0,0 +1,11 @@
+const { app } = require('@azure/functions');
+
+app.http('httpTrigger', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+        const name = request.query.get('name') || await request.text() || 'world';
+        return { body: `Hello, ${name} from Node.js Azure Function!` };
+    }
+});
